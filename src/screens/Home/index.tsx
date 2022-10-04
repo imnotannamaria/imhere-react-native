c5004ac1f974eaa28d733ef1edf1afb3eb4a04dc
Anna Maria <aannamariabr@outlook.com>
2022-10-04 13:57:53 -0300

    Continuação da criação da tela inicial

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,54 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export default function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Adicionou')
+  }
+
+  function handleParticipantRemove() {
+    console.log('Removeu')
+  }
+  
   return (
     <View style={styles.container}>
+      <StatusBar translucent style="light"/>
       <Text style={styles.eventName}>
         Nome do evento
       </Text>
 
       <Text style={styles.eventDate}
-      >
+      > 
         Segunda, 4 de Agosto de 2022.
       </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do participante'
+          placeholderTextColor='#6B6B6B'
+          // keyboardType='numeric'
+        /> 
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            + 
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant 
+        name='Anna Maria' 
+        onRemove={handleParticipantRemove}
+      />
+      <Participant 
+        name='Na Maria' 
+        onRemove={handleParticipantRemove}
+      />
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
       padding: 24,
   },
   eventName: {
-    color: '#FDFCFE',
+    color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16,
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  input: {
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    padding: 16,
+    fontSize: 18,
+    flex: 1,
+    marginRight: 12,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 25,
   }
 })
